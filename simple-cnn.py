de4304be0c2818027c5b33f679fe9b937b72b15b
Ubuntu <ubuntu@ip-172-31-38-50.ec2.internal>
2017-03-28 01:05:18 +0000

    fix(simple-cnn): Comment out import statement of visual packages

--- simple-cnn.py
+++ simple-cnn.py
@@ -2,8 +2,8 @@ import keras
 
 # Plot ad hoc CIFAR10 instances
 from keras.datasets import cifar10
-from matplotlib import pyplot
-from scipy.misc import toimage
+# from matplotlib import pyplot
+# from scipy.misc import toimage
 # load data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 # create a grid of 3x3 images
